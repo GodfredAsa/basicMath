575d635592d7409c3db7b2ae50d208325154bc8f
Godfred Asumadu Asamoah <godfred.asumadu.asamoah@gmail.com>
2021-08-07 16:52:44 +0000

    adds encapsulation OPP to the work flow

--- encapsulation.cpp
+++ encapsulation.cpp
@@ -5,24 +5,33 @@ class Employee {
 
     public: string firstName;
     public: string lastName;
-    public: int age;
+    public: string age;
     public: string programOfStudy;
   
-    private: int gpa;
+    // private: int gpa;
+
+    Employee(string fname, string lname, string myage, string program){
+        firstName = fname;
+        lastName = lname;
+        // age  =  myAge;
+        programOfStudy = program;
+    }
 
 // Setter
-    public: void set_gpa(double mygpa) {
-        gpa = mygpa;
-      }
-// Getter
-      int get_gpa() {
-        return gpa;
-      }
-};
+//     public: void set_gpa(double mygpa) {
+//         gpa = mygpa;
+//       }
+// // Getter
+//       int get_gpa() {
+//         return gpa;
+//       }
+// };
 
 int main() {
-    Employee truth;
-    truth.set_gpa(4.0);
-    cout<< "GPA: " << truth.get_gpa();
+    Employee truth("Truth", "Gadagoe", "36", "Science");
+    cout<<"\nEmployee Details \nName: " + truth.firstName + truth.lastName + "Age: "<<truth.age + "Program " + truth.programOfStudy;
+
+    // truth.set_gpa(4.0);
+    // cout<< "GPA: " << truth.get_gpa();
     return 0;
 }
