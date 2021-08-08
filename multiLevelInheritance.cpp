afc0e7eb8cbf891fc7643bc70c1056db9b9c5ca2
Godfred Asumadu Asamoah <godfred.asumadu.asamoah@gmail.com>
2021-08-08 19:45:01 +0000

    completes multilevel inheritance

--- multiLevelInheritance.cpp
+++ multiLevelInheritance.cpp
@@ -1,22 +1,34 @@
 #include<iostream>
 using namespace std;
 
+/*
+This is a multi-level inheritance where Vehicle is the base class, Toyota inherits Vehicle and Landcruiser also inherits from Toyota.
+*/
+// Base class
 class Vehicle{
     public: void starts(){
-
         cout<< "All vehicles are driven ";
-
     }
 };
 
 
+// sub class 
 class Toyota: public Vehicle{
+    public: string brand = "Toyota";
+};
 
-    public: string name = "Landcruiser";
+// sub class to Toyota and inherits it
+class Landcruiser: public Toyota{
+
 };
 
 
 int main(){
-
+//instance of Landcruiser class 
+    Landcruiser myFirst;
+//accessing the method in the base class
+    myFirst.starts();
+//accessing brand attribute in the base or super class
+    cout<<"\nThe brand name is: " + myFirst.brand;
     return 0;
 }
